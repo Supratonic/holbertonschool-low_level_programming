bf84ca552ac385543ea891d18d591ba0bae47ad4
supratonic <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@github.com>
2022-09-15 16:56:11 -0700

    Modified task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/*
+
+/**
  * main - Entry point
+ *
  * Return: always 0
  */
-int main(void)
+int main(void) /* main - Entry point */
 {
-	puts("Programming is like building a multilingual puzzle");
+	put(Programming is like building a multilingual puzzle);
 	return (0);
 }
