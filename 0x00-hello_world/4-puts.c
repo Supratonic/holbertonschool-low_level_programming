02314fccfaf81ecbcef9cd2a507003f88b30aab8
supratonic <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@github.com>
2022-09-15 15:38:46 -0700

    Fixed syntax on task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- *main 
- *Return Always 0
+ *main - entry point
+ *Return: always 0
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
