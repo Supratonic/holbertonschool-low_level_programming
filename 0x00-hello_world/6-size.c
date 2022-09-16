291156f635f16c307712ec496d65bf5118c9cd00
supratonic <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@github.com>
2022-09-16 09:25:23 -0700

    Fixed task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,16 @@
  */
 int main(void)
 {
-	printf("TypettttSize (bytes)");
-	printf("nCharacterttt    %ld", sizeof(char));
-	printf("nIntegertttt    %ld", sizeof(int));
-	printf("nLong intttt    %ld", sizeof(long int));
-	printf("nFloattttt    %ld", sizeof(float));
-	printf("nDoubletttt    %ld", sizeof(double));
-	printf("nLong doublettt    %ld", sizeof(long double));
+	char charType;
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
+
+	printf("Size of a char: %zu byte(s)/n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)/n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)/n", sizeof(longintType));
+	printf("Size of a long long int: %zu byte(s)/n", sizeof(longlongintType));
+	printf("Size of a float: %zu byte(s)/n", sizeof(floatType));
 	return (0);
 }
