1370142e41c111491fd0ce2ae209c33c00f91305
supratonic <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@github.com>
2022-09-15 17:00:59 -0700

    Testing task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@
  */
 int main(void) /* main - Entry point */
 {
-	put(Programming is like building a multilingual puzzle);
+	puts(Programming is like building a multilingual puzzle);
 	return (0);
 }
