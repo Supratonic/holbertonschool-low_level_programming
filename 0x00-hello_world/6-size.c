effd26a54e87e1eb76629e8a84457bae070d14ca
supratonic <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@github.com>
2022-09-15 17:23:08 -0700

    Added task 7

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	clrscr();
+	printf("TypettttSize (bytes)");
+	printf("nCharacterttt    %d", sizeof(char));
+	printf("nIntegertttt    %d", sizeof(int));
+	printf("nLong intttt    %d", sizeof(long int));
+	printf("nFloattttt    %d", sizeof(float));
+	printf("nDoubletttt    %d", sizeof(double));
+	printf("nLong doublettt    %d", sizeof(long double));
+	getch();
+	return (0);
+}
