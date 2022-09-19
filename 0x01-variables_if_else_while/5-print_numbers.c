ca801a58f0f59b511c6b5cef84eeb47a58f7e34f
supratonic <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@github.com>
2022-09-19 14:30:24 -0700

    Added task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints last digit of n and states whether
+ * it's greater than 5 or less than 6 and not 0
+ * Return: always 0
+*/
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+printf("%i", n);
+putchar('\n');
+return (0);
+}
