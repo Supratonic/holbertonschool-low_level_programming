b1866aed8f2d42942c72a6947160a550979b1b11
supratonic <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@github.com>
2022-09-19 13:50:49 -0700

    Added task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints last digit of n and states whether
+ * it's greater than 5 or less than 6 and not 0
+ * Return: always 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (c = 'A'; c <= 'Z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
