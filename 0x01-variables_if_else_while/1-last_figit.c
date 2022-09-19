92af08d9004215e457660d77007935a025d12855
supratonic <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@github.com>
2022-09-19 12:07:28 -0700

    Added task 1

---
+++ 0x01-variables_if_else_while/1-last_figit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints last digit of n and states whether it's greater than 5 or less than 6 and not 0.,
+ * Return: always 0
+*/
+int main(void)
+{
+	int n, a;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	a = n % 10;
+	if (a > 5)
+		printf("Last digit of %i is %i and is greater than 5 \n", n, a);
+	else if (a == 0)
+		printf("Last digit of %i is %i and is 0\n", n, a);
+	else (a < 6)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, a);
+	return (0);
+}
