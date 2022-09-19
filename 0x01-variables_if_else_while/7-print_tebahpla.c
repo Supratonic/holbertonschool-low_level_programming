061ae39efe9e16744edce2455e35303a7c31e2ea
supratonic <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@github.com>
2022-09-19 15:34:40 -0700

    Added task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints last digit of n and states whether
+ * it's greater than 5 or less than 6 and not 0
+ * Return: always 0
+*/
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+putchar(c);
+putchar('\n');
+return (0);
+}
