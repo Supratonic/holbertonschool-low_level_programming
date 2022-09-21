d04767766e312e3ccdc0e6faad3a863be0aa91df
supratonic <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@github.com>
2022-09-20 17:09:07 -0700

    Added task 0 and main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - entry of function
+*
+*
+*/
+int _putchar(c=_putchar)
+{
+	while (c != EOF) {
+		_putchar (c);
+		c = _putchar;
+	}
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Directory for 0x02-functions_nested_loops tasks
