3370e9148c6417449d4de89f26d18b9eccdc9d96
supratonic <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@github.com>
2022-09-20 21:33:49 -0700

    Added task 10-11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *add - will add two integers
+ *@num1: first integer
+ *@num2: second integer
+ *
+ * Return: sum of the two
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
