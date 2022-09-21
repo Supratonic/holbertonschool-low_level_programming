efc0ea1aa7ba8261e619feb5a5a9e4d9b3db4437
supratonic <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@github.com>
2022-09-20 21:36:24 -0700

    Fixed task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints alphabet in lowercase followed by a new line
- * Return: always 0 (success)
+ *print_alphabet - prints alphabet in lowercase followed by a new line
+ *Return: always 0 (success)
  */
 void print_alphabet(void)
 {
-	char a;
+	char letter;
 
-	for (a = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 
 	_putchar('\n');
